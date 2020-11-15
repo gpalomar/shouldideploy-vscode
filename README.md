260798750dd194653c72a7c6ab75c3dd09f69a5b
DustDev <guillem.palomar.agramunt@gmail.com>
2020-11-15 22:38:39 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# SHOULD I DEPLOY VSCODE 🍻⚠️
+
+This is a Visual Studio Code Extension that uses [https://github.com/baires/shouldideploy](shouldideploy.today) API endpoint [https://shouldideploy.today/api](https://shouldideploy.today/api).
+
+## Release
+
+See [CHANGELOG](CHANGELOG.md) for more information.
+
+## Contributing
+
+Bugs, feature requests and more are welcome here [GitHub Issues](https://github.com/gpalomar/shouldideploy-vscode/issues).
---
+++ src/extension.ts
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import * as moment from 'moment-timezone';
+import * as vscode from 'vscode';
+
+const timezone = moment.tz.guess();
+
+let myStatusBarItem: vscode.StatusBarItem;
+
+export function activate({ subscriptions }: vscode.ExtensionContext) {
+
+	// Register a command that is invoked when the status bar item is selected.
+	const myCommandId = 'shouldideploy.clickStatusBar';
+	subscriptions.push(vscode.commands.registerCommand(myCommandId, () => updateStatusBarItem()));
+
+	// Create a new status bar item that we can now manage.
+	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	myStatusBarItem.command = myCommandId;
+	subscriptions.push(myStatusBarItem);
+
+	// Register some listener that make sure the status bar item always up-to-date.
+	subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
+	subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem));
+
+	// Update status bar item once at start.
+	updateStatusBarItem();
+}
+
+interface IShoulDeployAPIResponse {
+	shouldideploy: boolean;
+	message: string;
+}
+
+async function updateStatusBarItem(): Promise<void> {
+	const response = await axios.get(`https://shouldideploy.today/api?tz=${timezone}`);
+	if(response.status === 200) {
+		const apiData = response.data as IShoulDeployAPIResponse;
+		if(!apiData.shouldideploy) {
+			myStatusBarItem.color = '#cc0000';
+		}
+		myStatusBarItem.text = `${apiData.shouldideploy ? '$(check)' : '$(warning)'} ${apiData.message.toUpperCase()}`;
+	} else {
+		myStatusBarItem.text = `$(megaphone) shouldideploy API error`;
+	}
+	myStatusBarItem.tooltip = 'Should I Deploy?';
+	myStatusBarItem.show();
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
